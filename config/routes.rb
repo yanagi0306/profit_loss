99c6d16642f7b978ec111c9ab1d78e2250d956e6
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-11-14 19:30:55 +0900

    サイドバー、ログイン時のヘッダー表示完成（仮）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :companies
   devise_for :stores
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'stores#index'
+
+  resources :stores,only: [:index]
+  resources :companies,only: [:index]
+
 end
