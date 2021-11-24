cae8f8938565756ea4b848455500e24d54b6e04e
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-11-24 22:38:40 +0900

    params取得完了

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,25 +1,41 @@
 class IncomesController < ApplicationController
   before_action :check
   before_action :today_date_getter
+  respond_to :html
 
   def index
     selected_instance_getter(@this_year, @this_month, current_store.id)
-
   end
 
   def search
     selected_instance_getter(params[:year], params[:month], current_store.id)
   end
-  # def updates
-  #   @incomes = Income.search(params[:year], params[:month], current_store)
-  #   selected_date_getter
-
-  # end
+  def updates
+    params_date
+    selected_instance_getter(
+      @params_ymd.year,
+      @params_ymd.month,
+      current_store.id,
+    )
+    @incomes =
+      income_params.to_unsafe_h.map do |id, income_params|
+        income = Income.find(id)
+        income.update_attributes(income_params)
+        income
+      end
+      redirect_to incomes_path
+    # respond_with(@incomes, location: search_incomes_path)
+  end
 
   private
 
+  def income_params
+    params.permit(incomes: [:id, :ymd, :income_category_id, :price])[:incomes]
+  end
 
-
+  def params_date
+    @params_ymd = Income.find(params[:incomes].keys[0])[:ymd]
+  end
 
   def today_date_getter
     @now = Date.today
@@ -27,17 +43,17 @@ class IncomesController < ApplicationController
     @this_year = @now.year
     @this_month = @now.month
     @year_range = current_store.opening_year..@this_year
-    @category_ids = IncomeCategory.data.map{|i| i[:id]}
-    @category_names =IncomeCategory.data.map{|i| i[:name]}
+    @category_ids = IncomeCategory.data.map { |i| i[:id] }
+    @category_names = IncomeCategory.data.map { |i| i[:name] }
   end
 
-  def selected_instance_getter(year,month,current_store)
+  def selected_instance_getter(year, month, current_store)
     @achievements = Income.search_getter(year, month, current_store)[0]
-    @selected_dates = @achievements.map{|i| i[:ymd]}
+    @selected_dates = @achievements.map { |i| i[:ymd] }
     @selected_year = @selected_dates[0].year
     @selected_month = @selected_dates[0].month
-    @incomes = Income.search_getter(year, month, current_store)
-    @incomes.delete_at(0)
+    @incomes_by_category = Income.search_getter(year, month, current_store)
+    @incomes_by_category.delete_at(0)
   end
 
   def check
