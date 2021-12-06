fd6b6b8125bd40c9518907914d52387642f2945a
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-12-06 17:56:16 +0900

    commit

--- app/models/variable_budgets.rb
+++ app/models/variable_budgets.rb
@@ -1,20 +1,20 @@
-class Fixed_costs < ActiveHash::Base
-  self.data = [
+# class Fixed_costs < ActiveHash::Base
+#   self.data = [
 
-    { id: 0, name: 'food_cost', ja_name: '食材費' },
-    { id: 1, name: 'material_cost', ja_name: '資材費' },
-    { id: 2, name: 'pert_cost', ja_name: 'P人件費' },
-    { id: 3, name: 'miscellaneous_cost', ja_name: '雑費' },
-    { id: 4, name: 'delivery_commission', ja_name: 'デリバリー手数料' },
-    { id: 5, name: 'electric', ja_name: '電気代' },
-    { id: 6, name: 'water', ja_name: '水道代' },
-    { id: 7, name: 'gas', ja_name: 'ガス代' },
-    { id: 8, name: 'power', ja_name: '動力代' },
-    { id: 9, name: 'overtime_employee_cost', ja_name: '社員残業代' },
-    { id: 10, name: 'social_insurance_part', ja_name: 'P社会保険' },
-    { id: 11, name: 'meeting', ja_name: '会議費' },
-    { id: 12, name: 'traveling', ja_name: '旅費交通費' },
-    { id: 13, name: 'selling_administration_cost', ja_name: '販売管理費' },
-    { id: 14, name: 'interest_payment', ja_name: '受取利息' },
-  ]
-end
+#     { id: 0, name: 'food_cost', ja_name: '食材費' },
+#     { id: 1, name: 'material_cost', ja_name: '資材費' },
+#     { id: 2, name: 'pert_cost', ja_name: 'P人件費' },
+#     { id: 3, name: 'miscellaneous_cost', ja_name: '雑費' },
+#     { id: 4, name: 'delivery_commission', ja_name: 'デリバリー手数料' },
+#     { id: 5, name: 'electric', ja_name: '電気代' },
+#     { id: 6, name: 'water', ja_name: '水道代' },
+#     { id: 7, name: 'gas', ja_name: 'ガス代' },
+#     { id: 8, name: 'power', ja_name: '動力代' },
+#     { id: 9, name: 'overtime_employee_cost', ja_name: '社員残業代' },
+#     { id: 10, name: 'social_insurance_part', ja_name: 'P社会保険' },
+#     { id: 11, name: 'meeting', ja_name: '会議費' },
+#     { id: 12, name: 'traveling', ja_name: '旅費交通費' },
+#     { id: 13, name: 'selling_administration_cost', ja_name: '販売管理費' },
+#     { id: 14, name: 'interest_payment', ja_name: '受取利息' },
+#   ]
+# end
