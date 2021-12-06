9b00edab42f220791884111d0311c60f182dcbc2
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-12-06 19:10:46 +0900

    commit

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,8 +1,8 @@
-FactoryBot.define do
-  factory :company do
-    name { "有限会社ティーツー"}
-    email { "aaaa@aaa.com" }
-    password { "aaaa1111" }
-    password_confirmation { "aaaa1111" }
-  end
-end
+# FactoryBot.define do
+#   factory :company do
+#     name { "有限会社ティーツー"}
+#     email { "aaaa@aaa.com" }
+#     password { "aaaa1111" }
+#     password_confirmation { "aaaa1111" }
+#   end
+# end
