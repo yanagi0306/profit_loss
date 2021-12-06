6ac3c4e3b228dd29aa7b0dfc4647d1c0c1c978bb
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-12-06 11:54:18 +0900

    commit

--- app/javascript/budget.js
+++
@@ -1,8 +0,0 @@
-const JapaneseHolidays = require("japanese-holidays");
-var today = new Date();
-var holiday = JapaneseHolidays.isHoliday(today);
-if (holiday) {
-  console.log("今日は " + holiday + " です");
-} else {
-  console.log("今日は祝日ではありません");
-}
