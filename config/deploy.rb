33fc15a9061d9cfad08251d38c49552897d071e6
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-12-07 15:56:04 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,21 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano (3.16.0)'
+lock '3.16.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'ご自身のアプリケーション名'
+set :application, 'profit_loss'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのユーザー名/レポジトリ名.git'
+set :repo_url,  'https://github.com/yanagi0306/profit_loss.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 'このアプリで使用しているrubyのバージョン' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/ご自身のキーペア名.pem']
+                                  keys: ['~/.ssh/profit_loss.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
