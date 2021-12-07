b4481df59473305e34af3593e19560261484da6c
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-12-07 16:17:36 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'profit_loss'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'https://github.com/yanagi0306/profit_loss.git'
+set :repo_url,  'git@github.com:yanagi0306/profit_loss.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
