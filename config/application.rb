1e2b91af98bc4c01a73f8b082ebf968c65431809
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2022-03-13 13:59:04 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,8 @@ module ProfitLoss
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = = 'asia/tokyo'
+
+    config.api_only = true
   end
 end
